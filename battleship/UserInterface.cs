3b8351e9c3372069b7a4f4b36239410e5617d88e
Matthew VH <vanheckematt@gmail.com>
2020-03-18 07:32:41 -0500

    Built method to GetUserInputFor method which is also capable of validating for int or char. Still need to include a prompt to ask for input. Also, haven't tested it yet, except that there are no syntactical errors.

---
+++ battleship/UserInterface.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace battleship
+{
+    static class UserInterface
+    {
+        public static string GetUserInputFor(string type)
+        {
+            string userInput = Console.ReadLine();
+            switch(type)
+            {
+                case "char":
+                    if(validateForChar(userInput))
+                    {
+                        return userInput;
+                    }
+                    else
+                    {
+                        return GetUserInputFor("char");
+                    }
+                case "int":
+                    if(validateForInt(userInput))
+                    {
+                        return userInput;
+                    }
+                    else
+                    {
+                        return GetUserInputFor("int");
+                    }
+            }
+            return userInput;
+        }
+        public static bool validateForInt(string userInput)
+        {
+            try
+            {
+                int userInputAsInt = int.Parse(userInput);
+                return true;
+            }
+            catch(Exception)
+            {
+                return false;
+            }
+        }
+        public static bool validateForChar(string userInput)
+        {
+            try
+            {
+                char userInputAsChar = char.Parse(userInput);
+                return true;
+            }
+            catch(Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
